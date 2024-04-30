fc71c95bfc29859792869d119d81620627b1d01e
Gabriel <144905801+Gabriel-OPereira@users.noreply.github.com>
2024-04-30 11:45:48 -0300

    Create main.py

---
+++ AP02/main.py
@@ -0,0 +1,24 @@
+# %%
+# imports
+from etl import ETL
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+# %%
+# gera os parâmetros para instanciar o objeto ETL
+usuario = os.getenv("USUARIO")
+senha = os.getenv("SENHA")
+host = os.getenv("HOST")
+banco_de_dados = os.getenv("BANCO_DE_DADOS")
+
+source = "dados.json"
+target = f"mssql+pymssql://{usuario}:{senha}@{host}/{banco_de_dados}"
+etl = ETL(source, target)
+# %%
+# testando
+etl.extract()
+etl.transform()
+etl.load()
