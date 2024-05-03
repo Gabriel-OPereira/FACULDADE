dad9d790dbf2079e0592584d5c20377b1a7cbf00
Gabriel <144905801+Gabriel-OPereira@users.noreply.github.com>
2024-05-03 08:10:18 -0300

    Update etl.py

--- AP02/etl.py
+++ AP02/etl.py
@@ -1,3 +1,5 @@
+
+ #%%  
 import json
 from abstract_etl import AbstractETL
 from classes import *
@@ -58,3 +60,44 @@ class ETL(AbstractETL):
             pc = PECA(numero = numero, status = status, inicio_fabricacao=inicio_fabricacao, fim_fabricacao = fim_fabricacao, numero_unidade_producao = numero_unidade_producao)
             session.add(pc)
         session.commit()
+
+        df_sp = self.transformed['SOPRADORA']
+        for indice,linha in df_pc.iterrows():
+            numero = linha['numero']
+            vazao_sopro=linha['vazao_sopro']
+            pressao_sopro = linha['pressao_sopro']
+            sp = SOPRADORA(numero = numero, vazao_sopro = vazao_sopro, pressao_sopro = pressao_sopro)
+            session.add(sp)
+        session.commit()
+
+        df_fs = self.transformed['FRESADORA']
+        for indice,linha in df_pc.iterrows():
+            numero = linha['numero']
+            velocidade_rotacao = linha['velocidade_rotacao']
+            profundidade_corte = linha['profundidade_corte']
+            fs = FRESADORA(numero = numero, velocidade_rotacao = velocidade_rotacao, profundidade_corte = profundidade_corte)
+            session.add(fs)
+        session.commit()
+
+        df_tc = self.transformed['TORNO_CNC']
+        for indice,linha in df_pc.iterrows():
+            numero = linha['numero']
+            velocidade_rotacao = linha['velocidade_rotacao']
+            tolerancia = linha['tolerancia']
+            tc = TORNO_CNC(numero = numero, velocidade_rotacao = velocidade_rotacao, tolerancia = tolerancia)
+            session.add(tc)
+        session.commit()
+
+        df_i3d = self.transformed['IMPRESSORA_3D']
+        for indice,linha in df_pc.iterrows():
+            numero = linha['numero']
+            espessura_camada = linha['espessura_camada']
+            tipo_material = linha['tipo_material']
+            i3d = IMPRESSORA_3D(numero = numero, espessura_camada = espessura_camada, tipo_material = tipo_material)
+            session.add(i3d)
+        session.commit()
+
+
+
+
+
