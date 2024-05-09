43042c933051a30c6cf558acef05520e34410df8
Gabriel <144905801+Gabriel-OPereira@users.noreply.github.com>
2024-05-09 11:27:27 -0300

    Create arquivo01.sql

---
+++ SQL SERVER/FUNCOES DE JANELA/arquivo01.sql
@@ -0,0 +1,33 @@
+--ex01
+
+SELECT 
+	f.Pnome,
+	f.Datanasc,
+	f.Salario,
+	SUM(f.Salario) OVER(ORDER BY f.Datanasc)
+FROM FUNCIONARIO f;
+
+--ex02
+
+SELECT 
+	f.Pnome,
+	f.Salario,
+	f.Dnr ,
+	AVG(f.Salario) OVER(PARTITION BY f.Dnr) 
+FROM FUNCIONARIO f
+
+--ex03
+
+SELECT 
+	f.Pnome,
+	COUNT(d.Fcpf) AS QUANTIDADE_DEP,
+	RANK() OVER(ORDER BY COUNT(d.Fcpf)) AS RANQUE
+FROM DEPENDENTE d 
+LEFT JOIN FUNCIONARIO f
+ON d.Fcpf = f.Cpf 
+GROUP BY  f.Pnome
+
+--ex04
+
+SELECT 
+FROM 
